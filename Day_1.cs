575523b4fad03ba4b31598762e883030010e13d2
Sam Damico <sdamico76@gmail.com>
2018-07-21 09:25:50 -0500

    Creation of solution, Completed Day 1 both parts with Unit tests

---
+++ Day_1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent {
+    public class Day_1 {
+        public static int CalculateSumOfRepeats(string input) {
+            var nums = input.Select(x => new String(x, 1)).ToList().Select(int.Parse);
+            var numsLinked = new LinkedList<int>(nums);
+            numsLinked.AddLast(numsLinked.First());
+            var sum = 0;
+            LinkedListNode<int> currentNode = numsLinked.First;
+            while(currentNode != null) {
+                if (currentNode.Value == currentNode.Next?.Value) {
+                    sum += currentNode.Value;
+                }
+                currentNode = currentNode.Next;
+            }
+            return sum;
+        }
+
+        public static int CalculateSumOfAntipodes(string input) {
+            var nums = input.Select(x => new String(x, 1)).ToList().Select(int.Parse).ToArray();
+            var half = nums.Count() / 2;
+            var sum = 0;
+
+            for (var i = 0; i < half; i++) {
+                if (nums[i] == nums[i + half]){
+                    sum += nums[i];
+                }
+            }
+            return sum * 2;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1 {
+namespace Advent {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("Day 1 Problem 0:");
+            Console.WriteLine(Day_1.CalculateSumOfRepeats(File.ReadAllText(@"C:\Personal Projects\Advent\ConsoleApp1\Day_1_0.txt")));
+
+            Console.WriteLine("Day 1 Problem 1:");
+            Console.WriteLine(Day_1.CalculateSumOfAntipodes(File.ReadAllText(@"C:\Personal Projects\Advent\ConsoleApp1\Day_1_0.txt")));
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
         }
     }
 }
