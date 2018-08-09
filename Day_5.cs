72783523f4c72d65c77fccec4ee8325eb91bff4c
Sam Damico <sdamico76@gmail.com>
2018-08-09 11:35:11 -0500

    Day 5 w/tests

---
+++ Day_5.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Advent {
+    public class Day_5 {
+        public static int TrampolineMaze(string input) {
+            var instructions = input.Split('\n').Select(int.Parse).ToArray();
+            var jumps = 0;
+            var currentLoc = 0;
+            while (true) {
+                if (currentLoc >= instructions.Length || currentLoc < 0) return jumps;
+                var oldLoc = currentLoc;
+
+                currentLoc += instructions[currentLoc];
+                instructions[oldLoc]++;
+
+                jumps++;
+            }
+        }
+
+        public static int TrampolineMazeWithNegative(string input) {
+            var instructions = input.Split('\n').Select(int.Parse).ToArray();
+            var jumps = 0;
+            var currentLoc = 0;
+            while (true) {
+                if (currentLoc >= instructions.Length || currentLoc < 0) return jumps;
+                var oldLoc = currentLoc;
+
+                currentLoc += instructions[currentLoc];
+                if(instructions[oldLoc] >= 3) {
+                    instructions[oldLoc]--;
+                } else {
+                    instructions[oldLoc]++;
+                }
+
+                jumps++;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,30 +8,16 @@ using System.Threading.Tasks;
 namespace Advent {
     class Program {
         static void Main(string[] args) {
-            //Console.WriteLine("Day 1 Problem 0:");
-            //Console.WriteLine(Day_1.CalculateSumOfRepeats(File.ReadAllText(@"C:\Personal Projects\Advent\ConsoleApp1\Day_1_0.txt")));
-
-            //Console.WriteLine("Day 1 Problem 1:");
-            //Console.WriteLine(Day_1.CalculateSumOfAntipodes(File.ReadAllText(@"C:\Personal Projects\Advent\ConsoleApp1\Day_1_0.txt")));
-
-            //Console.WriteLine("Day 2 Problem 0:");
-            //Console.WriteLine(Day_2.CorruptionChecksum(File.ReadAllText(@"C:\Personal Projects\Advent\ConsoleApp1\Day_2_0.txt")));
-
-            //Console.WriteLine("Day 2 Problem 1:");
-            //Console.WriteLine(Day_2.CorruptionChecksum_EvenlyDivisible(File.ReadAllText(@"C:\Personal Projects\Advent\ConsoleApp1\Day_2_0.txt")));
-
-            //Console.WriteLine("Day 3 Problem 0:");
-            //Console.WriteLine(Day_3.GetSpiralLocation(368078));
-
-            //Console.WriteLine("Day 2 Problem 1:");
-            //Console.WriteLine(Day_3.GetSpiralLocationPart2(368078));
-
-            //Console.WriteLine("Day 4 Problem 0:");
-            //Console.WriteLine(Day_4.HighEntropyPassphrases(File.ReadAllText(@"C:\Personal Projects\Advent\ConsoleApp1\Day_4_0.txt")));
-
-            Console.WriteLine("Day 4 Problem 1:");
-            Console.WriteLine(Day_4.HighEntropyPassphrasesAnagram(File.ReadAllText(@"C:\Personal Projects\Advent\ConsoleApp1\Day_4_0.txt")));
-
+            Console.WriteLine("Day 1 Problem 0:" + Day_1.CalculateSumOfRepeats(File.ReadAllText(@"C:\Personal Projects\Advent\ConsoleApp1\Day_1_0.txt")));
+            Console.WriteLine("Day 1 Problem 1:" + Day_1.CalculateSumOfAntipodes(File.ReadAllText(@"C:\Personal Projects\Advent\ConsoleApp1\Day_1_0.txt")));
+            Console.WriteLine("Day 2 Problem 0:" + Day_2.CorruptionChecksum(File.ReadAllText(@"C:\Personal Projects\Advent\ConsoleApp1\Day_2_0.txt")));
+            Console.WriteLine("Day 2 Problem 1:" + Day_2.CorruptionChecksum_EvenlyDivisible(File.ReadAllText(@"C:\Personal Projects\Advent\ConsoleApp1\Day_2_0.txt")));
+            Console.WriteLine("Day 3 Problem 0:" + Day_3.GetSpiralLocation(368078));
+            Console.WriteLine("Day 2 Problem 1:" + Day_3.GetSpiralLocationPart2(368078));
+            Console.WriteLine("Day 4 Problem 0:" + Day_4.HighEntropyPassphrases(File.ReadAllText(@"C:\Personal Projects\Advent\ConsoleApp1\Day_4_0.txt")));
+            Console.WriteLine("Day 4 Problem 1:" + Day_4.HighEntropyPassphrasesAnagram(File.ReadAllText(@"C:\Personal Projects\Advent\ConsoleApp1\Day_4_0.txt")));
+            Console.WriteLine("Day 5 Problem 0:" + Day_5.TrampolineMaze(File.ReadAllText(@"C:\Personal Projects\Advent\ConsoleApp1\Day_5_0.txt")));
+            Console.WriteLine("Day 5 Problem 0:" + Day_5.TrampolineMazeWithNegative(File.ReadAllText(@"C:\Personal Projects\Advent\ConsoleApp1\Day_5_0.txt")));
 
 
 
